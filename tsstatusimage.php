0c02111025735470efe28911c567b82f6291ab69
Rafael Arkenau <r.arkenau@googlemail.com>
2014-07-09 23:01:37 +0200

    Update tsstatusimage.php

--- tsstatusimage.php
+++ tsstatusimage.php
@@ -2,12 +2,12 @@
 
 header('Content-Type: image/png');	// Content-Type im HTTP-Response-Header setzen
 
-if($socket = @fsockopen('speedy.a40gaming.de', 10011, $errno, $errstr, 2)) { // IF: Teamspeak-Server erreichbar
-	$image = 'http://static.a40gaming.de/ts/online.png';
+if($socket = @fsockopen('Add IP of TS-Server here', 10011, $errno, $errstr, 2)) { // IF: Teamspeak-Server erreichbar
+	$image = 'online.png';
 }
 
 else {	//ELSE: Teamspeak-Server nicht erreichbar
-	$image = 'http://static.a40gaming.de/ts/offline.png';
+	$image = 'offline.png';
 }
 
 @stream_set_timeout($socket, 4); //Timeout für Netzwerkverbindung festlegen
